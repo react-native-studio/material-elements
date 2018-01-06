9c202e41086700f7488ab9c60c8d2f1194d18f6b
lmy <2534290808@qq.com>
2018-01-06 12:28:23 +0800

    badge增加主题

--- src/Badge/Badge.react.js
+++ src/Badge/Badge.react.js
@@ -1,22 +1,27 @@
+
 /* eslint-disable import/no-unresolved, import/extensions */
+
 import { Text, View } from 'react-native';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 /* eslint-enable import/no-unresolved, import/extensions */
 import Icon from '../Icon';
-
+import { ViewPropTypes } from '../utils';
+import getTheme from '../styles/getTheme';
+import merge from 'lodash/merge';
+import light from '../styles/themes/light'
 const propTypes = {
     /**
-    * The badge will be added relativelty to this node
-    */
+     * The badge will be added relativelty to this node
+     */
     children: PropTypes.node,
     /**
-    * This is the content rendered within the badge
-    */
+     * This is the content rendered within the badge
+     */
     text: PropTypes.string,
     /**
-    * When the icon is set, the content will be <Icon name={icon} /> element
-    */
+     * When the icon is set, the content will be <Icon name={icon} /> element
+     */
     icon: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.shape({
@@ -26,51 +31,49 @@ const propTypes = {
         }),
     ]),
     /**
-    * Just sugar for style={{ container: { width: size, height: size, borderRadius: size / 2 }}}
-    */
+     * Just sugar for style={{ container: { width: size, height: size, borderRadius: size / 2 }}}
+     */
     size: PropTypes.number,
     /**
-    * You can specify stroke for badge. Note that if you use stroke it swaps container and
-    * strokeContainer. So if you override styles of container you probably need to override
-    * strokeContainer instead the container. Because if you use stroke then the strokeContainer
-    * will be wrapper of whole badge component.
-    */
+     * You can specify stroke for badge. Note that if you use stroke it swaps container and
+     * strokeContainer. So if you override styles of container you probably need to override
+     * strokeContainer instead the container. Because if you use stroke then the strokeContainer
+     * will be wrapper of whole badge component.
+     */
     stroke: PropTypes.number,
+    style: PropTypes.shape({
+        container: ViewPropTypes.style,
+    }),
 };
 const defaultProps = {
     children: null,
     text: null,
     icon: null,
-    size: 4,
+    size: 16,
     stroke: null,
     style: {
         container: {
             top: -8,
             right: -8,
         },
+
     },
-};
-const contextTypes = {
-    uiTheme: PropTypes.object.isRequired,
-};
 
-function getStyles(props, context) {
-    const { badge, palette } = context.uiTheme;
+};
+function getStyles(props, theme) {
+    const { badge} = getTheme(theme);
+    const {palette}=merge(light,merge);
     const { accent, size, stroke } = props;
-
     const local = {
         container: {},
         strokeContainer: {},
     };
-
     if (size && stroke) {
         const strokeSize = size;
         const contentSize = size - stroke;
-
         local.strokeContainer.width = strokeSize;
         local.strokeContainer.height = strokeSize;
         local.strokeContainer.borderRadius = strokeSize / 2;
-
         local.container.position = null;
         local.container.width = contentSize;
         local.container.height = contentSize;
@@ -80,11 +83,9 @@ function getStyles(props, context) {
         local.container.height = size;
         local.container.borderRadius = size / 2;
     }
-
     if (accent) {
         local.container.backgroundColor = palette.accentColor;
     }
-
     return {
         container: [
             badge.container,
@@ -103,6 +104,7 @@ function getStyles(props, context) {
         ],
     };
 }
+
 const mapIconProps = ({ icon, size }) => {
     let iconProps = {};
 
@@ -112,16 +114,22 @@ const mapIconProps = ({ icon, size }) => {
         iconProps = icon;
     }
 
+
     if (!iconProps.size && size) {
         iconProps.size = size / 2;
+
     }
 
+
     return iconProps;
+
 };
+
 
 class Badge extends PureComponent {
     constructor(props, context) {
         super(props, context);
+
 
         this.renderContent = this.renderContent.bind(this);
         this.renderChildren = this.renderChildren.bind(this);
@@ -129,7 +137,9 @@ class Badge extends PureComponent {
     renderContent(styles) {
         const { text, icon, stroke } = this.props;
 
+
         let content = null;
+
 
         if (icon) {
             const iconProps = mapIconProps(this.props);
@@ -138,36 +148,44 @@ class Badge extends PureComponent {
             content = <Text style={styles.content}>{text}</Text>;
         }
 
+
         const contentWrapper = (
             <View style={styles.container}>
                 {content}
             </View>
         );
 
+
         if (!stroke) {
             return contentWrapper;
         }
+
 
         return (
             <View style={styles.strokeContainer}>
                 {contentWrapper}
             </View>
         );
+
     }
     renderChildren() {
         const { children } = this.props;
+
 
         if (!children) {
             return null;
         }
 
+
         return children;
+
     }
     render() {
-        const styles = getStyles(this.props, this.context);
+        const styles = getStyles(this.props, this.props.theme);
+
 
         return (
-            <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexDirection: 'row'}}>
                 {this.renderChildren()}
                 {this.renderContent(styles)}
             </View>
@@ -175,8 +193,10 @@ class Badge extends PureComponent {
     }
 }
 
+
 Badge.propTypes = propTypes;
 Badge.defaultProps = defaultProps;
-Badge.contextTypes = contextTypes;
+
+
 
 export default Badge;
