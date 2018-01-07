ed3985ed31f4d5a2494a105aa1f632680ab730d4
lmy <2534290808@qq.com>
2018-01-07 13:21:21 +0800

    Icon增加FontAwesome，Ionicons，materialCommunityIcon三种类型

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,31 +1,52 @@
 /* eslint-disable import/no-unresolved, import/extensions */
 import VectorIcon from 'react-native-vector-icons/MaterialIcons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import IoniconIcon from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIconsIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import getTheme from '../styles/getTheme'
 /* eslint-enable import/no-unresolved, import/extensions */
-
+const iconType={
+    MaterialIcons:'MaterialIcons',
+    FontAwesome:'FontAwesome',
+    Ionicons:'Ionicons',
+    MaterialCommunityIcons:'MaterialCommunityIcons'
+}
 const propTypes = {
     name: PropTypes.string.isRequired,
     style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
     size: PropTypes.number,
     color: PropTypes.string,
+    type:PropTypes.string,
 };
 const defaultProps = {
     size: null,
     color: null,
     style: null,
+    type:iconType.MaterialIcons
 };
 class Icon extends PureComponent {
+
+    _getIconComponent=()=>{
+        let type=this.props.type,{MaterialCommunityIcons,FontAwesome,Ionicons,MaterialIcons}=iconType
+        switch (type){
+            case MaterialIcons:return VectorIcon;break;
+            case FontAwesome:return FontAwesomeIcon;break;
+            case Ionicons:return IoniconIcon;break;
+            case MaterialCommunityIcons:return MaterialCommunityIconsIcon;break;
+            default:return VectorIcon;break;
+        }
+    }
     render() {
         const { name, style, size, color } = this.props;
         const { palette, spacing } = getTheme({});
 
         const iconColor = color || palette.secondaryTextColor;
         const iconSize = size || spacing.iconSize;
-
+        let IconComponent=this._getIconComponent();
         return (
-            <VectorIcon
+            <IconComponent
                 name={name}
                 size={iconSize}
                 color={iconColor}
@@ -37,4 +58,5 @@ class Icon extends PureComponent {
 
 Icon.propTypes = propTypes;
 Icon.defaultProps = defaultProps;
+Icon.iconType=iconType;
 export default Icon;
