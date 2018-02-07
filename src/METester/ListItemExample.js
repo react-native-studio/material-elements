ba5279ec4a8f99ddbbc5e3be5f56a00d45f9095d
2534290808 <2534290808@qq.com>
2018-02-07 17:18:03 +0800

    update listitem example
    Signed-off-by: 2534290808 <2534290808@qq.com>

--- src/METester/ListItemExample.js
+++ src/METester/ListItemExample.js
@@ -6,11 +6,11 @@ import ListItem from '../ListItem/ListItem.flow'
 export default class ListItemExample extends Component{
   render(){
     return(<View>
-      <ListItem  leftElement={{name:'arrow-back'}}/>
-      <ListItem  leftElement={{name:'arrow-back'}} centerElement="title"/>
-      <ListItem  leftElement={{name:'arrow-back'}} centerElement={{primaryText:'title',secondaryText:'second'}}/>
-      <ListItem  leftElement={{name:'arrow-back'}} rightElement={{menu:{labels:['1','2','3','4']}}}/>
-      <ListItem  leftElement={{name:'arrow-back'}} centerElement="title"
+      <ListItem divider onPress={()=>{}} leftElement={{name:'arrow-back'}}/>
+      <ListItem divider onPress={()=>{}}  leftElement={{name:'arrow-back'}} centerElement="title"/>
+      <ListItem divider onPress={()=>{}}  leftElement={{name:'arrow-back'}} centerElement={{primaryText:'title',secondaryText:'second'}}/>
+      <ListItem divider onPress={()=>{}}  leftElement={{name:'arrow-back'}} rightElement={{menu:{labels:['1','2','3','4']}}}/>
+      <ListItem divider onPress={()=>{}}  leftElement={{name:'arrow-back'}} centerElement="title"
                  rightElement={[{name:'done'},{name:'arrow-back'},{name:'menu'}]}/>
 
     </View>)
