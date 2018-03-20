dc0f7213795ad45ceb82fc6c385ffb7cf7893eb1
2534290808 <2534290808@qq.com>
2018-03-20 14:36:58 +0800

    添加 Imagebackground以适应RN版本变化
    Signed-off-by: 2534290808 <2534290808@qq.com>

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,10 +3,13 @@ import {
     BackHandler,
     ViewPropTypes as RNViewPropTypes,
     BackAndroid as DeprecatedBackAndroid,
+    ImageBackground as RNImageBackground,
+    Image
 } from 'react-native';
 
 const ViewPropTypes = RNViewPropTypes || View.propTypes;
 const BackAndroid = BackHandler || DeprecatedBackAndroid;
+const ImageBackground = RNImageBackground || Image;
 const MotionCurve=require('./MotionCurve');
 const MotionDuration=require('./MotionDuration');
 
@@ -15,4 +18,5 @@ export {
     BackAndroid,
     MotionCurve,
     MotionDuration,
+    ImageBackground,
 };
