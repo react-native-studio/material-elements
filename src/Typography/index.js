dc6873cf0fcfa2f46bf9864800b1e5d53675d89e
2534290808 <2534290808@qq.com>
2018-03-21 14:01:40 +0800

    添加Typography
    Signed-off-by: 2534290808 <2534290808@qq.com>

---
+++ src/Typography/index.js
@@ -0,0 +1,25 @@
+/**
+ * 来自于https://github.com/hectahertz/react-native-typography.git
+ * 详情请到对应的github 项目中查看
+ */
+export { default as human } from "./collections/human";
+export { default as humanDense } from "./collections/humanDense";
+export { default as humanTall } from "./collections/humanTall";
+export { default as material } from "./collections/material";
+export { default as materialTall } from "./collections/materialTall";
+export { default as materialDense } from "./collections/materialDense";
+export { default as iOSUIKit } from "./collections/iOSUIKit";
+export { default as iOSUIKitDense } from "./collections/iOSUIKitDense";
+export { default as iOSUIKitTall } from "./collections/iOSUIKitTall";
+
+export { default as sanFranciscoWeights } from "./helpers/sanFranciscoWeights";
+export { default as robotoWeights } from "./helpers/robotoWeights";
+export { default as notoCJKWeights } from "./helpers/notoCJKWeights";
+export { default as notoTallWeights } from "./helpers/notoTallWeights";
+export { default as webWeights } from "./helpers/webWeights";
+export { default as systemWeights } from "./helpers/systemWeights";
+export { default as systemDenseWeights } from "./helpers/systemDenseWeights";
+export { default as systemTallWeights } from "./helpers/systemTallWeights";
+export { default as sanFranciscoSpacing } from "./helpers/sanFranciscoSpacing";
+export { default as iOSColors } from "./helpers/iOSColors";
+export { default as materialColors } from "./helpers/materialColors";
