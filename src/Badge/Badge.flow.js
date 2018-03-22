507754690d02f1ff7ac914ee40d4ea995c5dacab
2534290808 <2534290808@qq.com>
2018-03-22 12:34:27 +0800

    badge的container自适应text内容变化   修改BadgeExample
    Signed-off-by: 2534290808 <2534290808@qq.com>

--- src/Badge/Badge.flow.js
+++ src/Badge/Badge.flow.js
@@ -3,24 +3,38 @@
  */
 import { Text, View } from 'react-native';
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import Icon from '../Icon';
 import { ViewPropTypes } from '../utils';
 import getTheme from '../styles/getTheme';
 import merge from 'lodash/merge';
 import light from '../styles/themes/light'
-import type { IconPropTypes } from '../TypeDifinition/index'
+import type {IconPropTypes} from "../Icon/Icon.flow";
 
-type StyleType={
+type BadgeStyle={
   container?:ViewPropTypes.style,
   outerContainer?:ViewPropTypes.style,
   content?:ViewPropTypes.style,
 }
-type BadgeProps={
+type BadgePropTypes={
+  /**
+   * 子元素
+   */
   children:mixed,
+  /**
+   * badge所展示文字
+   */
   text?:string,
+  /**
+   * badge所展示icon
+   */
   icon?:IconPropTypes,
-  style?:StyleType,
+  /**
+   * badge样式
+   */
+  style?:BadgeStyle,
+  /**
+   * badge强调色
+   */
   accent?:boolean,
 }
 const defaultProps = {
@@ -33,11 +47,11 @@ const defaultProps = {
     },
 
 };
-function getStyles(props:BadgeProps) {
+function getStyles(props:BadgePropTypes) {
     let { badge} = getTheme();
     const {palette}=merge(light,merge);
     const { accent} = props;
-    const local:StyleType = {
+    const local:BadgeStyle = {
          container:{
          }
     };
@@ -66,76 +80,83 @@ function getStyles(props:BadgeProps) {
 }
 
 const mapIconProps = ({ icon }) => {
-
     let iconProps={size:16};
     if(icon){
-      let {name,type,size,color}=icon&&icon;
+      let {name,type,size,color}=icon && icon;
       iconProps = merge(
         {size:16},
-        {
-          name,
-          type,
-          size,
-          color,
+        {name,type,size,color,
         });
     }
-
     return iconProps;
-
 };
 
 
-class Badge extends PureComponent<BadgeProps> {
-
-    props:BadgeProps
+class Badge extends PureComponent<BadgePropTypes> {
+    props:BadgePropTypes
     static defaultProps:typeof defaultProps=defaultProps
-    constructor(props:BadgeProps) {
-        super(props);
 
-        console.log(props)
+    _container:any
 
-        this.renderContent = this.renderContent.bind(this);
-        this.renderChildren = this.renderChildren.bind(this);
+    _width:number=0
+
+    //TODO，此处需要注意onLayout的使用注意事项
+    //只有在元素的宽高不确定的情况下，onLayout才能得出真是的宽高，即元素的style不设置width，height属性，
+    //其父元素也不能设置宽高，因为父元素设置宽高之后，也间接确定了子元素的宽高
+    onTextLayout=(e:any):void=>{
+      let width=0;
+      if (e.nativeEvent.layout.width <= e.nativeEvent.layout.height) {
+        width = e.nativeEvent.layout.height+10
+      } else {
+        width = e.nativeEvent.layout.width + 15
+      }
+      if (this._width == width) {
+        return
+      }
+      this._width = width;
+      let height=e.nativeEvent.layout.height+10;
+      let borderRadius = height / 2;
+      this._container&&this._container.setNativeProps({
+        style: {
+          width,
+          height,
+          borderRadius,
+        },
+      });
     }
-    renderContent=(styles:StyleType)=> {
+
+    renderContent=(styles:BadgeStyle)=> {
         const { text, icon } = this.props;
-
-        let content = null;
-
+        let content = null,atachStyle={};
         if (icon) {
             const iconProps = mapIconProps(this.props);
             content = <Icon {...iconProps} />;
         } else if (text) {
-            content = <Text style={styles.content}>{text}</Text>;
+            atachStyle={width:undefined,height:undefined}
+            content = <Text onLayout={this.onTextLayout} style={styles.content}>{text}</Text>;
         }
-
-
         const contentWrapper = (
-            <View style={styles.container}>
+            <View ref={view=>this._container=view} style={[styles.container,atachStyle]}>
                 {content}
             </View>
         );
-
         return contentWrapper
-
     }
     renderChildren=() =>{
         const { children } = this.props;
-
         if (!children) {
             return null;
         }
-
        return children;
 
     }
     render() {
         const styles = getStyles(this.props);
         return (
-            <View style={styles.outerContainer}>
+           <View style={styles.outerContainer}>
                 {this.renderChildren()}
                 {this.renderContent(styles)}
-            </View>
+           </View>
         );
     }
 }
--- src/METester/BadgeExmaple.js
+++ src/METester/BadgeExmaple.js
@@ -5,9 +5,28 @@ import Badge from '../Badge'
 export default class BadgeExmaple extends Component{
   render(){
     return(<View style={{flex:1,paddingVertical:20}}>
-      <Badge text="12">
-        <Text>Badge</Text>
+      <Badge style={{
+        outerContainer:styles.badge1
+      }} text="12345sdfsdfsdf">
+        <Text>BadgeExample</Text>
+      </Badge>
+      <Badge style={{outerContainer:styles.badge2}} accent text="1">
+        <Text>BadgeExample</Text>
+      </Badge>
+      <Badge style={{outerContainer:styles.badge2}} accent icon={{
+        name:'arrow-back',color:'white'
+      }}>
+        <Text>BadgeExample</Text>
       </Badge>
     </View>)
   }
 }
+const styles=StyleSheet.create({
+  badge1:{
+    width:130
+  },
+  badge2:{
+    width:110,
+    marginTop:20
+  }
+})
