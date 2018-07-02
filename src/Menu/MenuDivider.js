2a2ac55903f5839af15b0ece155bc05e644e3e01
2534290808 <2534290808@qq.com>
2018-07-02 11:22:25 +0800

    MenuDivider user flow

--- src/Menu/MenuDivider.js
+++ src/Menu/MenuDivider.js
@@ -1,12 +1,22 @@
+/**
+ * @flow
+ */
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import PropTypes from 'prop-types';
-
-const MenuDivider = ({ color }) => (
-  <View style={[styles.divider, { borderBottomColor: color }]} />
-);
-
+type MenuDividerProps={
+  color?:string
+}
+class MenuDivider extends React.Component<MenuDividerProps>{
+  static defaultProps:MenuDividerProps
+  render(){
+    let {color} = this.props;
+    return (
+      <View style={[styles.divider, { borderBottomColor: color }]} />
+    )
+  }
+}
 MenuDivider.defaultProps = {
   color: 'rgba(0,0,0,0.12)',
 };
