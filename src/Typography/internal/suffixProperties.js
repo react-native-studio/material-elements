95f54390a02617d5afd96336404adb5b30f10bae
黎明 <2534290808@qq.com>
2019-03-05 11:27:16 +0800

    删除一些不必要的文件

--- src/Typography/internal/suffixProperties.js
+++
@@ -1,7 +0,0 @@
-const suffixProperties = (object, suffix) =>
-  Object.keys(object).reduce(
-    (acc, key) => ({ ...acc, [`${key}${suffix}`]: object[key] }),
-    {}
-  );
-
-export default suffixProperties;
